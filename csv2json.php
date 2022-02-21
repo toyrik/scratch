efb20c7ccf9b8f3506ce6ee337555b9c39bcab1b
toyrik <toyrik@gmail.com>
2022-02-21 21:24:42 +0300

    add csv2json.php

---
+++ csv2json.php
@@ -0,0 +1,24 @@
+<?php
+
+if (($handle = fopen("data.csv", "r")) !== FALSE) {
+    $csvs = [];
+    while(! feof($handle)) {
+       $csvs[] = fgetcsv($handle);
+    }
+    $datas = [];
+    $column_names = [];
+    foreach ($csvs[0] as $single_csv) {
+        $column_names[] = $single_csv;
+    }
+    foreach ($csvs as $key => $csv) {
+        if ($key === 0) {
+            continue;
+        }
+        foreach ($column_names as $column_key => $column_name) {
+            $datas[$key-1][$column_name] = $csv[$column_key];
+        }
+    }
+    $json = json_encode($datas);
+    fclose($handle);
+    print_r($json);
+}
